d41f49add2967e9457ec283424ab82f55debea36
Daniel Mrva <mrva.paints@gmail.com>
2022-04-04 17:43:03 -0600

    added seed data, associations.  still not sure if we need replies

---
+++ seeds/seed.js
@@ -0,0 +1,17 @@
+const sequelize = require('../config/connection');
+const {User, Post} = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({force:true});
+
+    const user = await User.bulkCreate(userData);
+
+    const post = await Post.bulkCreate(postData);
+
+    process.exit(0);
+};
+
+seedDatabase();
